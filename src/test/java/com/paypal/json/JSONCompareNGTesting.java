79247a25bb09dcdbb013973053473a8a4a959505
Packiaraj <packya@gmail.com>
2021-05-14 20:03:28 +0530

    Update JSONCompareNGTesting.java

--- src/test/java/com/paypal/json/JSONCompareNGTesting.java
+++ src/test/java/com/paypal/json/JSONCompareNGTesting.java
@@ -1,54 +1,81 @@
 package com.paypal.json;
 
-import org.json.JSONException;
-import org.junit.Assert;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.apache.poi.xssf.usermodel.XSSFCell;
+import java.util.List;
 
-import org.apache.poi.xssf.usermodel.XSSFRow;
-
-import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.junit.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import au.com.bytecode.opencsv.CSVReader;
 
 public class JSONCompareNGTesting {
 
 	@DataProvider(name = "data-provider")
-    public Object[][] dataProviderMethod() {
-        return new Object[][] { { "localFile.json" , "productionFile.json" } };
-    }
-	
-	@Test(dataProvider = "data-provider", singleThreaded = true)
-    public void testMethod(String localFile, String prodFile) throws Exception {
-		String expected = JSONAssert.readFileAsString(localFile);
-    	System.out.println(expected);
+	public Object[][] dataProviderMethod() {
+		return new Object[][] { { "localFile.json", "productionFile.json" } };
+	}
 
-        String actual = JSONAssert.readFileAsString(prodFile);
-        System.out.println(actual);
-    	Assert.assertNotNull(actual);
-    	
-    	JSONAssert.assertEquals(expected, actual, true);
-    }
-	
+	//@Test(dataProvider = "data-provider", singleThreaded = true)
+	public void testMethod(String localFile, String prodFile) throws Exception {
+		String expected = JSONAssert.readFileAsString(localFile);
+		System.out.println(expected);
+
+		String actual = JSONAssert.readFileAsString(prodFile);
+		System.out.println(actual);
+		Assert.assertNotNull(actual);
+
+		JSONAssert.assertEquals(expected, actual, true);
+	}
+
 	@DataProvider(name = "excel-data")
 	public Object[][] excelDP() throws Exception {
 		// We are creating an object from the excel sheet data by calling a method that
 		// reads data from the excel stored locally in our system
-		Object[][] arrObj = getExcelData("/data/test.xlsx");
+		Object[][] arrObj = getExcelData("/data/test.csv");
 		return arrObj;
 	}
+
 	
-		@Test(dataProvider = "excel-data")
+
+	private Object[][] getExcelData(String fileName) throws IOException {
+		
+		 CSVReader reader = new CSVReader(new FileReader(fileName), ',', '"', 1);
+	       
+	      //Read all rows at once
+	    //  List<String[]> allRows = reader.readAll();
+	       
+	      
+	     
+	     
+	    
+	     List<String[]> csvLines=reader.readAll();
+	     for (int index=0; index < csvLines.size(); index++) {
+	       String[] line=csvLines.get(index);
+	       
+	       return new Object[][] { { line[0], line[1] }}; 
+	    
+	       
+	     }
+	     
+	   
+		return null;
+	}
+
+	
+	/*
+	 * @Test(dataProvider = "excel-data") public void testCompareJSON(String
+	 * localFile, String prodFile) throws Exception { String expected =
+	 * JSONAssert.readFileAsString(localFile); System.out.println(expected);
+	 * 
+	 * String actual = JSONAssert.readFileAsString(prodFile);
+	 * System.out.println(actual); Assert.assertNotNull(actual);
+	 * 
+	 * JSONAssert.assertEquals(expected, actual, true); }
+	 */
+
+	@Test(dataProvider = "excel-data")
 	public void testCompareJSONURL(String url1, String url2) throws Exception {
 		// String expected = JSONAssert.getJSON(url1);
 		// System.out.println(expected);
@@ -62,6 +89,5 @@ public class JSONCompareNGTesting {
 
 		// JSONAssert.assertEquals(expected, actual, true);
 	}
-	
-	
+
 }
